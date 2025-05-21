bceddecfbe6aa79926e462d6245ef91e4e3f5abd
FMontes-alt <franciscomontes95@gmail.com>
2025-05-21 13:38:20 +0200

    Introducir el audio de Nobita

--- js/audioPersonaje.js
+++ js/audioPersonaje.js
@@ -1,18 +1,6 @@
 'use strict';
 
-function activarAudioModal(modalId, audioId) {
-    const modal = document.getElementById(modalId);
-    const audio = document.getElementById(audioId);
-
-    if (!modal || !audio) return;
-
-    modal.addEventListener('shown.bs.modal', () => {
-        audio.currentTime = 0;
-        audio.play();
-    });
-
-    modal.addEventListener('hidden.bs.modal', () => {
-        audio.pause();
-        audio.currentTime = 0;
-    });
+function playNobitaAudio() {
+    const audio = new Audio('../assets/audio/audioNobita.mp3');
+    audio.play();
 }
